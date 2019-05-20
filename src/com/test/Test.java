ce290fd231685f9f4e671f21d710dd2c3bf3c091
huangzhifeng <huang@qq.com>
2019-05-20 15:10:50 +0800

    高鼎修改了

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,6 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("属兔");
+		System.out.println("属兔");
+		System.out.println("属兔");
+		System.out.println("属兔");
+		System.out.println("属兔");
 	}
 
 }
